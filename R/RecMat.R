b6f6b8c76bbbe5e7b13328354981851c3d10215a
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-03-25 12:09:18 -0700

    added cp,,emt

--- R/RecMat.R
+++ R/RecMat.R
@@ -1,4 +1,7 @@
 
+# should accept any matrix implementation for which %*% and as.matrix()
+# are defined
+
 newrecmat <- function(mat,mattype=c('mat','sparsemat','distmat') {
    tmp <- list()
    tmp$recmat <- mat
