0c08c1a139d37290c801f5475b5775c0c4536d90
Norm Matloff <matloff@cs.ucdavis.edu>
2016-03-25 21:10:05 -0700

    allow factors in buildMatrix

--- R/buildMatrix.R
+++ R/buildMatrix.R
@@ -1,11 +1,17 @@
 
-buildMatrix <- function(data){
-users = data[,1]
-movies = data[,2]
-rating = data[,3]
-newMatrix = matrix(0, nrow = max(users), ncol = max(movies))
-for(rows in 1:nrow(data)){
-    newMatrix[data[rows,1],data[rows,2]] = data[rows,3]
-}
-return (newMatrix)
+buildMatrix <- function(data,naVal=0){
+   # deal with possible factors
+   dmax <- function(d) {
+      if (is.factor(d)) return(length(levels(d)))
+      max(d)
+   }
+   users = data[,1]
+   movies = data[,2]
+   rating = data[,3]
+   newMatrix = matrix(naVal, 
+          nrow = dmax(users), ncol = dmax(movies))
+   for(rows in 1:nrow(data)){
+       newMatrix[data[rows,1],data[rows,2]] = data[rows,3]
+   }
+   return (newMatrix)
 }
