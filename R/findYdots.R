58b4aeaf9bee4a42ccf31331ee1d7a40574fc9c2
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-04-17 15:26:51 -0700

    minor changes to find.., fixing up xval...

--- R/findYdots.R
+++ R/findYdots.R
@@ -1,14 +1,50 @@
-findYdots <- function(ratingsIn){
+
+# arguments:
+
+#   ratingsIn: raw data, with each row of the form (user, item, rating)
+#   keepUsrs: if TRUE, keep split by users
+#   keepItms: if TRUE, keep split by items
+
+# value:
+
+#   S3 class of type "ydots", with components:
+
+#      Y..: grand mean
+#      Yi.: vector of mean ratings for each user
+#      Y.j: vector of mean ratings for each item
+#      grpByUsrs: a list showing all the items rated by each user
+#      grpByItms: a list showing all the users who  rated each item
+
+findYdots <- function(ratingsIn,keepUsrs=FALSE,keepItms=FALSE){
   users = ratingsIn[,1]
-  effect = ratingsIn[,2]
-  value = ratingsIn[,3]
-  Y.. = mean(value) # this is Y..
-  Yi. = tapply(value,users,mean) # this is the mean of all ratings per user
-  Y.j = tapply(value,effect,mean) #this is the mean of all ratings per movie
-  means <- structure(list(Y..,Yi.,Y.j), class= "means")
-  names(means) = c("Overall Mean", "Mean Values per user", "Mean Values per Effect")
-  return(means)
+  items = ratingsIn[,2]
+  ratings = ratingsIn[,3]
+  Y.. = mean(ratings) # this is Y..
+  if (keepUsrs) {
+     grpByUsrs = split(items,users)
+     Yi. = sapply(grpByUsrs,mean)
+  } else
+     Yi. = tapply(ratings,users,mean) # means of all ratings per user
+  if (keepItms) {
+     grpByItms = split(users,items)
+     Y.j = sapply(grpByItms,mean)
+  } else
+     Y.j = tapply(ratings,items,mean) # means of all ratings per item
+  ydots = list(GrandMean=Y..,UsrMeans=Yi.,ItmMeans=Y.j)
+  if (keepUsrs) ydots$grpByUsrs = grpByUsrs
+  if (keepItms) ydots$grpByItms = grpByItms
+  class(ydots) = 'ydots'
+  ydots
 } 
 
+# check
+checkyd <- function() {
+   check <- 
+      data.frame(userID = c(1,3,2,1,2),itemID = c(1,1,3,2,3),ratings=6:10)
+   print(check)
+   print(findYdots(check))
+   print(findYdots(check,keepUsrs=T))
+   print(findYdots(check,keepItms=T))
+}
 
 
--- R/xvalBiases.R
+++ R/xvalBiases.R
@@ -1,35 +1,35 @@
-xvalBiases <- function(ratingsIn, accmeasure){
+
+xvalBiases <- function(ratingsIn, accmeasure='exact'){
   # Split into training and validation sets 
   rowNum = nrow(ratingsIn)/2
-  trainingSet = as.matrix(ratingsIn[1:rowNum ,]) 
+  trainingSet = ratingsIn[1:rowNum ,]
   trainRatings = trainingSet[,3]
-  trainMovies = trainingSet[,2]
+  trainItems = trainingSet[,2]
   trainUsers = trainingSet[,1]
+  # get means
   means = findYdots(trainingSet)
-  # Calculate all the means
-  Y.. = means$`Overall Mean`
-  Yi. = means$`Mean Values per user`
-  Yj. = means$`Mean Values per Effect`
-  
-  testA = as.matrix(ratingsIn[rowNum:nrow(ratingsIn),])
-  
-  for(i in 1:nrow(trainingA)){
-    testA[i,3] = Yi.[testA[i,1]] + Y.j[testA[i,2]] - Y..
+  Y.. = means$GrandMean
+  Yi. = means$UsrMeans
+  Y.j = means$ItmMeans
+  testA = ratingsIn[(rowNum+1):nrow(ratingsIn),]
+  # predict the cases in the test set
+  testA$pred = 
+     Yi.[as.character(testA[,1])] + Y.j[as.character(testA[,2])] - Y..
+  # calculate accuracy 
+  if (accmeasure == 'exact') {
+     testA$pred = round(testA$pred)
+     return(mean(testA$pred == testA[,3],na.rm=TRUE))
+  } else if (accmeasure == 'mad') {
+     return(mean(abs(testA$pred-testA[,3]),na.rm=TRUE))
   }
-  testA = round(testA)
-  # Lets calculate the accuracy 
-  counter = 0
-  for(row in 1:nrow(testA))
-    if(testA[row,3] == ratingsIn[row,3]){
-      counter = counter + 1
-    }
-  accuracy = counter/length(trainA)
-  if(accuracy < accmeasure)
-    accurate = FALSE
-  else(accuracy >= accmeasure)
-    accurate = TRUE
-  acc <- structure(list(accurate,testA))
-  names(acc) = c("Accuracy", "NewSet")
-  return(acc)
 }
+
+# check
+checkxv <- function(acc='mad') {
+   check <- 
+      data.frame(userID = c(1,3,2,1,2,3),itemID = c(1,1,3,2,3,3),ratings=5:10)
+   print(check)
+   print(xvalBiases(check,acc))
+}
+
 
