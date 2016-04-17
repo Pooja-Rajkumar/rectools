0d62e65b9973298599030b6ff66ed5cac2a3fc33
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-04-17 15:37:43 -0700

    added some doc for xv...

--- R/xvalBiases.R
+++ R/xvalBiases.R
@@ -1,7 +1,20 @@
 
-xvalBiases <- function(ratingsIn, accmeasure='exact'){
+# splits input data into training and test sets, fits "ydots" model on
+# the former, then predicts the latter
+
+# arguments:
+
+#   ratingsIn: input data frame, with first cols (userID,itemID,rating)
+#   trainprop: proportion of data for the training set
+#   accmeasure: accuracy measure
+
+# value:
+
+#    accuracy value
+
+xvalBiases <- function(ratingsIn, trainprop=0.5,accmeasure='exact'){
   # Split into training and validation sets 
-  rowNum = nrow(ratingsIn)/2
+  rowNum = floor(trainprop * nrow(ratingsIn))
   trainingSet = ratingsIn[1:rowNum ,]
   trainRatings = trainingSet[,3]
   trainItems = trainingSet[,2]
@@ -15,6 +28,7 @@ xvalBiases <- function(ratingsIn, accmeasure='exact'){
   # predict the cases in the test set
   testA$pred = 
      Yi.[as.character(testA[,1])] + Y.j[as.character(testA[,2])] - Y..
+print(testA)
   # calculate accuracy 
   if (accmeasure == 'exact') {
      testA$pred = round(testA$pred)
@@ -25,11 +39,11 @@ xvalBiases <- function(ratingsIn, accmeasure='exact'){
 }
 
 # check
-checkxv <- function(acc='mad') {
+checkxv <- function(trainprop=0.5,acc='mad') {
    check <- 
       data.frame(userID = c(1,3,2,1,2,3),itemID = c(1,1,3,2,3,3),ratings=5:10)
    print(check)
-   print(xvalBiases(check,acc))
+   print(xvalBiases(check,trainprop,acc))
 }
 
 
