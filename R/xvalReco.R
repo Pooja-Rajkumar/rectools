464d932cc9edfb921c8038e294dfcb1f32bfd931
Pooja-Rajkumar <pooja.rajkumar123@gmail.com>
2016-04-29 12:11:46 -0700

    Updated xvalreco
    Receiving warning: In apred == testSet[, 3] :
      longer object length is not a multiple of shorter object length 
    Will fix later

--- R/xvalReco.R
+++ R/xvalReco.R
@@ -2,22 +2,36 @@
 # return: apred, a predicted data set from transposing recosystems P and Q
 #
 #
-xvalReco <- function(newdata,testx,testy)
+xvalReco <- function(newdata, trainprop = 0.5,
+                     accmeasure = c('exact','mad','rms'))
 {
   library(recosystem)
   r <- Reco()
-  trainRow =  floor(nrow(newdata))/2
-  trainidxs = newdata[1:trainRow,1:3]
-  testRow = nrow(newdata) - trainRow
-  testidxs = newdata[1:testRow,1:3]
-  write.table(trainidxs,file = "train.txt", row.names = FALSE, col.names = FALSE)
-  write.table(testidxs,file = "test.txt", row.names = FALSE, col.names = FALSE)
+  trainRow = floor(trainprop*nrow(newdata))
+  trainidxs = sample(1:nrow(newdata),trainRow)
+  trainSet = newdata[trainidxs,]
+  testSet = newdata[setdiff(1:nrow(newdata),trainidxs),]
+  write.table(trainSet,file = "train.txt", row.names = FALSE, col.names = FALSE)
+  write.table(testSet,file = "test.txt", row.names = FALSE, col.names = FALSE)
   r$train('train.txt')
   res <- r$output(NULL,NULL)
   p <- res$P
   q <- res$Q
-  apred <- p %*% t(q) 
-  print(head(testidxs))
-  print(head(apred[testx,testy])) 
-  apred
+  apred <- p %*% t(q)
+  numpredna = sum(is.na(apred))
+  accmeasure = match.arg(accmeasure)
+  result = list(ndata =nrow(newdata),trainprop = trainprop, 
+                accmeasure = accmeasure, numpredna = numpredna)
+  if(accmeasure == 'exact'){
+    apred = round(apred)
+    acc = mean(apred == testSet[,3],na.rm = TRUE)
+  } else if (accmeasure == 'mad'){
+    acc = mean(abs(apred-testSet[,3]), na.rm = TRUE)
+  } else if (accmeasure == 'rms'){
+    acc = sqrt(mean((apred-testSet[,3])^2,na.rm = TRUE))
+    
+  }
+  result$acc = acc 
+  class(result) <- 'xvalreco'
+  result
 }
