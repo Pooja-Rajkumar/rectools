af4d0a0b0d9729ec10128e3df405ebb030fd3c87
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-04-30 12:44:39 -0700

    findYdotsMLE.R working

--- R/findYdotsMLE.R
+++ R/findYdotsMLE.R
@@ -1,6 +1,4 @@
 
-WORK IN PROGRESS
-
 # arguments:
 
 #   ratingsIn: input data, with first cols (userID,itemID,rating,
@@ -19,10 +17,9 @@ WORK IN PROGRESS
 #      regOjb: if have covariates, object of class 'lm' from
 #              the regression op
 
-findYdotsMLE <- function(ratingsInName,cls=NULL) {
+findYdotsMLE <- function(ratingsIn,cls=NULL) {
   require(partools)
   if (is.null(cls)) {
-     ratingsIn <- get(ratingsInName)
      users = ratingsIn[,1]
      items = ratingsIn[,2]
      ratings = ratingsIn[,3]
@@ -31,19 +28,15 @@ findYdotsMLE <- function(ratingsInName,cls=NULL) {
      if (!haveCovs) {
         tmp = sprintf('%s ~ (1|%s) + (1|%s)',
            nms[3],nms[1],nms[2])
-        frml = as.formula(paste(tmp)
+        frml = as.formula(paste(tmp))
         lmerout = lmer(frml,data=ratingsIn)
         Y.. = fixef(lmerout)
-     } else Y.. = mean(ratings) 
-     clm <- coef(lmerout)
-clm$V1[,1][196]  # est. alpha_196
-clm$V2[,1][242]  # est. beta_242
-
-     Yi. = tapply(ratings,users,mean) # means of all ratings per user
-     Y.j = tapply(ratings,items,mean) # means of all ratings per item
+        clm = coef(lmerout)
+        Yi. = clm[[nms[1]]][,1]
+        Y.j = clm[[nms[2]]][,1]
+     } 
   } else {
-     # find haveCovs
-     stop('paralel feature not implemented yet')
+     stop('parallel feature not implemented yet')
   }
   ydots = list(grandMean=Y..,usrMeans=Yi.,itmMeans=Y.j)
   if (haveCovs) ydots$regObj = lmout
@@ -56,10 +49,11 @@ checkyd <- function() {
    check <- 
       data.frame(userID = c(1,3,2,1,2),itemID = c(1,1,3,2,3),ratings=6:10)
    print(check)
-   print(findYdots(check))
-   check$cv <- c(1,4,6,2,10)
-   print(check)
-   print(findYdots(check))
+   print(findYdotsMLE(check))
+        browser()
+   # check$cv <- c(1,4,6,2,10)
+   # print(check)
+   # print(findYdotsMLE(check))
 }
 
 
