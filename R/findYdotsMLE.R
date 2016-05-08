3728b9127712761001436317cc72f2dde126fea5
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-05-08 15:23:51 -0700

    still on parallel MLE

--- R/findYdotsMLE.R
+++ R/findYdotsMLE.R
@@ -11,55 +11,64 @@
 
 #   S3 class of type "ydots", with components:
 
-#      Y..: grand mean (0 if have covariates)
+#      Y..: grand mean (fixef() if have covariates)
 #      Yi.: vector of mean ratings for each user
+#           (part of ranef() if have covariates)
 #      Y.j: vector of mean ratings for each item
+#           (part of ranef() if have covariates)
 
 findYdotsMLE <- function(ratingsIn,cls=NULL) {
   require(lme4)
   nms <- names(ratingsIn)
   haveCovs = ncol(ratingsIn) > 3
-  if (is.null(cls)) {
-     if (!haveCovs) {
-        tmp = sprintf('%s ~ (1|%s) + (1|%s)',
-           nms[3],nms[1],nms[2])
-        frml = as.formula(paste(tmp))
-     } else {
-        frml <- paste(nms[3],'~ ')
-        for (i in 4:ncol(ratingsIn)) {
-           frml <- paste(frml,nms[i])
-           frml <- paste(frml,'+')
-        }
-        frml <- paste(frml,'(1|',nms[1],')',
-                         '+ (1|',nms[2],')')
-        frml <- as.formula(frml)
-      }
-    } else {
-       require(partools)
-    }
-    lmerout = lmer(frml,data=ratingsIn)
-    ydots = list()
-    if (!haveCovs) {
-       Y.. = fixef(lmerout)
-       ydots$Y.. = Y..
-       clm = coef(lmerout)
-       Yi. = clm[[nms[1]]][,1]
-       names(Yi.) = as.character(unique(ratingsIn[,1]))
-       ydots$Yi. = Yi.
-       Y.j = clm[[nms[2]]][,1]
-       names(Y.j) = as.character(unique(ratingsIn[,2]))
-       ydots$Y.j = Y.j
-    } else {
-       ydots$Y.. = fixef(lmerout)
-       tmp = ranef(lmerout)
-       ydots$Yi. = tmp[[2]][1][,1]
-       ydots$Y.j = tmp[[1]][1][,1]
-    }
+  if (!haveCovs) {
+     tmp = sprintf('%s ~ (1|%s) + (1|%s)',
+        nms[3],nms[1],nms[2])
+     frml = as.formula(paste(tmp))
   } else {
-     stop('parallel feature not implemented yet')
+     frml <- paste(nms[3],'~ ')
+     for (i in 4:ncol(ratingsIn)) {
+        frml <- paste(frml,nms[i])
+        frml <- paste(frml,'+')
+     }
+     frml <- paste(frml,'(1|',nms[1],')',
+                      '+ (1|',nms[2],')')
+     frml <- as.formula(frml)
+  }
+  if (is.null(cls)) {
+     lmerout = lmer(frml,data=ratingsIn)
+     ydots = formYdots(ratingsIn,nms,haveCovs)
+  } else {
+     require(partools)
+     distribsplit(cls,'ratingsIn')
+     clusterExport(cls,c('frml','formYdots'),envir=environment())
+     clusterEvalQ(cls,lmerout = lmer(frml,data=ratingsIn))
+     ydots = clusterEvalQ(cls,formYdots(ratingsIn,nms,haveCovs))
+     class(ydots) = 'ydotsMLEpar'
+  }
+  invisible(ydots)
+}
+
+formYdots = function(ratingsIn,nms,haveCovs) {
+  ydots = list()
+  if (!haveCovs) {
+     Y.. = fixef(lmerout)
+     ydots$Y.. = Y..
+     clm = coef(lmerout)
+     Yi. = clm[[nms[1]]][,1]
+     names(Yi.) = as.character(unique(ratingsIn[,1]))
+     ydots$Yi. = Yi.
+     Y.j = clm[[nms[2]]][,1]
+     names(Y.j) = as.character(unique(ratingsIn[,2]))
+     ydots$Y.j = Y.j
+  } else {
+     ydots$Y.. = fixef(lmerout)
+     tmp = ranef(lmerout)
+     ydots$Yi. = tmp[[2]][1][,1]
+     ydots$Y.j = tmp[[1]][1][,1]
   }
   class(ydots) = 'ydotsMLE'
-  invisible(ydots)
+  ydots
 }
 
 # predict() method for the 'ydotsMLE' class
