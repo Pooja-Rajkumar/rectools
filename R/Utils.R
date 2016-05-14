9d1cf89b05e73ca457c550da7c58d36aed66c832
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-05-14 14:57:23 -0700

    added ratingness() utility

--- R/Utils.R
+++ R/Utils.R
@@ -1,2 +1,9 @@
 
+# finds and returns the number of ratings for each user (splitCol = 1)
+# or each item (splitCol = 2), for the input data frame ratingsIn, in
+# UserID | ItemID | Rating ...  format
+
+ratingness <- function(ratingsIn,splitCol) {
+   tapply(ratingsIn[,3],ratingsIn[,splitCol],length)
+}
 
