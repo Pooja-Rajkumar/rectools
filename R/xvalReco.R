2c867ca221f074689688164125274cb8004c7a06
Pooja-Rajkumar <pooja.rajkumar123@gmail.com>
2016-05-18 01:42:34 -0700

    Added new training function, added option to rank

--- R/xvalReco.R
+++ R/xvalReco.R
@@ -1,22 +1,37 @@
-xvalReco <- function(ratingsIn, trainprop = 0.5,
-                     accmeasure = c('exact','mad','rms'))
-{
+training <- function(trainSet,
+                    trainprop = 0.5,
+                    rank = 10){
   library(recosystem)
   r <- Reco()
-  rownew = nrow(ratingsIn)
-  trainRow = floor(trainprop*rownew)
-  trainidxs = sample(1:rownew,trainRow)
-  trainSet = ratingsIn[trainidxs,]
-  testSet = ratingsIn[setdiff(1:nrow(ratingsIn),trainidxs),]
   write.table(trainSet,file = "train.txt", row.names = FALSE, col.names = FALSE)
-  r$train('train.txt')
+  r$train('train.txt',opts = list(dim=rank))
   res <- r$output(NULL,NULL)
-  testSet$pred <- vector(length=nrow(testSet))
-  for(i in 1:nrow(testSet)){
-    j = ratingsIn[i,1]
-    k = ratingsIn[i,2]
-    testSet$pred[i] = res$P[j,] %*% res$Q[k,]
-  }
+  res
+}
+xvalReco <- function(ratingsIn, trainprop = 0.5,
+                     accmeasure = c('exact','mad','rms'),
+                     cls = NULL,
+                     rank = 10)
+{
+  library(recosystem)
+  library(parallel)
+  if(is.null(cls)){
+    rownew = nrow(ratingsIn)
+    trainRow = floor(trainprop*rownew)
+    trainidxs = sample(1:rownew,trainRow)
+    trainSet = ratingsIn[trainidxs,]
+    testSet = ratingsIn[setdiff(1:nrow(ratingsIn),trainidxs),]
+    res = training(ratingsIn)
+    p = res$P
+    q = res$Q
+    testSet$pred <- vector(length=nrow(testSet))
+    for(i in 1:nrow(testSet)){
+      j = ratingsIn[i,1]
+      k = ratingsIn[i,2]
+      testSet$pred[i] = p[j,] %*% q[k,]
+    }
+  }else {
+    }
   numpredna = sum(is.na(testSet$pred))
   accmeasure = match.arg(accmeasure)
   result = list(ndata =nrow(ratingsIn),trainprop = trainprop, 
@@ -31,8 +46,7 @@ xvalReco <- function(ratingsIn, trainprop = 0.5,
     acc = sqrt(mean((testSet$pred-testSet[,3])^2,na.rm = TRUE))
   }
   result$acc = acc 
-  result$pred = testSet$pred
-  result$testVals = testSet[,3]
   class(result) <- 'xvalreco'
   result
 }
+
