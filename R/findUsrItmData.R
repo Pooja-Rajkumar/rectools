6507baed6265b813a5c6749e1349f8d2b89f5a65
Norm Matloff <matloff@cs.ucdavis.edu>
2016-06-22 22:45:29 -0700

    more on user data

--- R/findUsrItmData.R
+++ R/findUsrItmData.R
@@ -1,6 +1,9 @@
 
-# utilities to read in raw data and form two R lists, one for users and
-# one for items, of classes 'usrData' and 'itmData'
+# note: it is assumed that user IDs are consecutive numbers starting at
+# 1
+
+# utilities to read in raw data and form an R list for user data,
+# with class 'usrData' 
 
 # arguments:
 
@@ -8,8 +11,8 @@
 #               and rating 
 #    usrCovs: data frame of user covariates, e.g. gender and age, one
 #             row per user
-#    itmCatCols: numbers of columns in ratingsIn containing item
-#                categories, e.g. movie genre; data values are 0 or 1
+#    itmCovs: data frame of item categories, e.g. genre for movies,
+#             row per item; categories need not be mutually exclusive
 #    fileOut: if specified, save the value returned by the function
 #             using R save(), with file name fileOut
 
@@ -19,14 +22,19 @@
 #    each such element is itself an R list, with these components:
 #
 #       ratings: ratings set by this user
-#       covars:  covariate data for this user, if any
+#       cvrs:  covariate data for this user, if any
+#       itms:  item category data for this user, if any; i-th element
+#              is proportion of items rated by this user that are 
+#              in category i
 
-formUserData <- function(ratingsIn,usrCovs=NULL,itmCatCols,fileOut='') {
-   usrs <- split(ratingsIn[,3],ratingsIn[,1])
-   retval <- list()
+formUserData <- function(ratingsIn,usrCovs=NULL,itmCats-NULL,fileOut='') {
    class(retval) <- 'usrData'
+   # rownums[[i]] will be the row numbers in ratingsIn belonging to user i
+   rownums <- split(1:nrow(ratingsIn),ratingsIn[,1])
+   nusers <- length(rownums)
+   retval <- list(length=nusers)
    for (i in 1:length(usrs)) {
-      retval[[i]] <- list(ratings=usrs[[i]])
+      retval[[i]]$ratings <- split(ratingsIn[,3],ratingsIn[,1])
       if (!is.null(covCols))
    }
 
