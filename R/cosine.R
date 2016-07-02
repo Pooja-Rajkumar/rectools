0cfb8f4f7a1da620af29655be3a8c5a60bd69ec0
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2016-07-02 11:19:28 -0700

    true cos now

--- R/cosine.R
+++ R/cosine.R
@@ -35,14 +35,14 @@ predict.usrData <- function(origData,newData,newItem,
    # tmp
    if (is.null(whoHasIt)) return(NA)  # no one rated this item
    origData <- origData[whoHasIt]
-   found <- found[,whoHasIt]
+   found <- found[,whoHasIt,drop=FALSE]
    onecos <- function(y) cosDist(newData,y,wtcovs,wtcats)
-   dists <- sapply(origData,onecos) 
-   k1 <- min(k,sum(!is.na(dists)))
-   ksmall <- order(dists)[1:k1]
-   # ksmall is a vector containing the indices (with respect to
+   cosines <- sapply(origData,onecos) 
+   k1 <- min(k,sum(!is.na(cosines)))
+   klarge <- order(cosines,decreasing=TRUE)[1:k1]
+   # klarge is a vector containing the indices (with respect to
    # origData) of the k closest users to newData
-   mean(found[2,ksmall])
+   mean(found[2,klarge])
 }
 
 # cosDist() find cosine distance between x and y, elements of an object
@@ -53,12 +53,20 @@ cosDist <- function(x,y,wtcovs,wtcats) {
    # where are they in x and y?
    xwhere <- which(!is.na(match(x$itms,commItms)))
    ywhere <- which(!is.na(match(y$itms,commItms)))
-   cosTot <- x$ratings[xwhere] %*% y$ratings[ywhere]
+   xrats <- x$ratings[xwhere]
+   yrats <- y$ratings[ywhere]
+   cosTot <- xrats %*% yrats
+   xl2 <- sum(xrats^2)
+   yl2 <- sum(yrats^2)
    if (!is.null(wtcovs)) {
       cosTot <- cosTot + wtcovs * x$cvrs %*% y$cvrs
+      xl2 <- xl2 + sum((wtcovs*x$cvrs)^2)
+      yl2 <- yl2 + sum((wtcovs*y$cvrs)^2)
    }
    if (!is.null(wtcats)) {
       cosTot <- cosTot + wtcats * x$cats %*% t(y$cats)
+      xl2 <- xl2 + sum((wtcovs*x$cats)^2)
+      yl2 <- yl2 + sum((wtcovs*y$cats)^2)
    }
-   cosTot
+   cosTot / sqrt(xl2 * yl2)
 }
