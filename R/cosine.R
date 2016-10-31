1c3a333b73e716585520bd7d4287d808e6142b39
Norm Matloff <matloff@cs.ucdavis.edu>
2016-10-30 23:20:35 -0700

    improved comments and code

--- R/cosine.R
+++ R/cosine.R
@@ -1,9 +1,11 @@
 
 # rating prediction via nearest neighbors, via "cosine" (inner product);
-# the latter, though standard, has certain problems, and other choices
-# for distance measure will be added
+# the latter, though standard, has certain problems (e.g., its
+# scale-free nature), and other choices for distance measure will be
+# added
 
-# covariates and item type preferences may be used
+# covariates (e.g. age, gender) and item type preferences (e.g.
+# preferred movie genres) may be used
 
 ########################## predict() function ##########################
 
@@ -32,7 +34,8 @@ predict.usrData <- function(origData,newData,newItem,
    #
    # action of checkNewItem(): here oneUsr is one user record in
    # origData; the function will check which item in the record, if any,
-   # equals newItem
+   # equals newItem, and will return that value and the corresponding
+   # rating; defined for use by sapply() below
    checkNewItem <- function(oneUsr) {
       tmp <- match(oneUsr$itms,newItem)
       if (all(is.na(tmp))) return(c(NA,NA))
@@ -40,20 +43,28 @@ predict.usrData <- function(origData,newData,newItem,
       c(whichOne,oneUsr$ratings[whichOne])
    }
    found <- as.matrix(sapply(origData,checkNewItem))
-   # found[i] = j means origData[[i]]$itms[j] = newItem;
-   # found[i] = NA means newItem wasn't rated by user i;
-   # we need to get rid of the users who have this as NA
+   # found is of dimensions 2 x number of users;
+   # found[1,i] = j means origData[[i]]$itms[j] = newItem;
+   # found[1,i] = NA means newItem wasn't rated by user i;
+   # found[2,i] will be the rating in the non-NA case
+   # we need to get rid of the NA users 
    whoHasIt <- which(!is.na(found[1,]))
    # whoHasIt[i] is the index, i.e. user ID, of the i-th user who has
    # rated newData
    if (is.null(whoHasIt)) return(NA)  # no one rated this item
-   origData <- origData[whoHasIt]  
+   origData <- origData[whoHasIt,]  
    # now origData only has the relevant users, the ones who have rated
-   # newItem
+   # newItem, so select only those columns of the found matrix
    found <- found[,whoHasIt,drop=FALSE]
+   # find the distance from newData to one user y of origData; defined for
+   # use in sapply() below
    onecos <- function(y) cosDist(newData,y,wtcovs,wtcats)
    cosines <- sapply(origData,onecos) 
-   k1 <- min(k,sum(!is.na(cosines)))
+   # the vector cosines contains the distances from newData to all the
+   # original data points;
+   # now find the k nearest neighbors; first,
+   # can't have more neighbors than the whole data set
+   k1 <- min(k,length(cosines))  
    klarge <- order(cosines,decreasing=TRUE)[1:k1]
    # klarge is a vector containing the indices (with respect to
    # origData) of the k closest users to newData
@@ -61,10 +72,12 @@ predict.usrData <- function(origData,newData,newItem,
 }
 
 # cosDist() find cosine distance between x and y, elements of an object
-# of 'usrData' class; wtcovs, wtcats as in cosprep()
+# of 'usrData' class; only items rated in both x and y are used; if none
+# exist, then return NaN
 cosDist <- function(x,y,wtcovs,wtcats) {
    # rated items in common
    commItms <- intersect(x$itms,y$itms)
+   if (is.null(commItms)) return(Nan)
    # where are they in x and y?
    xwhere <- which(!is.na(match(x$itms,commItms)))
    ywhere <- which(!is.na(match(y$itms,commItms)))
