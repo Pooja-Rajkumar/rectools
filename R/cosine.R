0c7b0e4b81e53c8927b4e1418a6504622ed2e30f
vchakrab <vch@ucdavis.edu>
2016-11-28 01:45:15 -0800

    added cosine.Rd
    comment changes in cosine.R

--- R/cosine.R
+++ R/cosine.R
@@ -1,34 +1,28 @@
 
-# rating prediction via nearest neighbors, via "cosine" (inner product);
-# the latter, though standard, has certain problems (e.g., its
-# scale-free nature), and other choices for distance measure will be
-# added
-
-# covariates (e.g. age, gender) and item type preferences (e.g.
-# preferred movie genres) are allowed
-
 ########################## predict() function ##########################
 
-# predict newData argument from origData argument
 
-# arguments:
+#'  @title predict newData argument from origData argument
+#'
+#'  rating prediction via nearest neighbors, via "cosine" (inner product);
+#'  the latter, though standard, has certain problems (e.g., its
+#'  scale-free nature), and other choices for distance measure will be added.
+#'  covariates (e.g. age, gender) and item type preferences (e.g.
+#'  preferred movie genres) are allowed
+#'
+#' @param origData: training set, object of class 'usrData' (see file findUsrItmData.R)
+#' @param newData: data point (just one for now) to be predicted, object of class 'usrDatum'
+#' @param newItem: ID of the item rating to be predicted for the user in newData
+#' @param wtcovs: weight to put on covariates; NULL if no covs
+#' @param wtcats: weight to put on item categories; NULL if no cats
+#' @k: a list of the number of nearest neigbhors to be considered while predicting
+#'
+#' @return predicted ratings for newData
 
-#    origData: training set, object of class 'usrData' (see file
-#    findUsrItmData.R)
-#    newData: data point (just one for now) to be predicted, object of
-#             class 'usrDatum'
-#    newItem: ID of the item rating to be predicted for the user in
-#             newData
-#    wtcovs: weight to put on covariates; NULL if no covs
-#    wtcats: weight to put on item categories; NULL if no cats
-#    k: a list of the number of nearest neigbhors to be considered while predicting
-
-# value:
-
-#    predicted ratings for newData
 
 predict.usrData <- function(origData,newData,newItem,
       k,wtcovs=NULL,wtcats=NULL) {
+  UseMethod("predict.usrData")
    # we need to narrow origData down to the users who have rated newItem
    #
    # action of checkNewItem(): here oneUsr is one user record in
@@ -36,26 +30,26 @@ predict.usrData <- function(origData,newData,newItem,
    # equals newItem, and will return that value and the corresponding
    # rating; defined for use by sapply() below
    checkNewItem <- function(oneUsr) {
-browser()
      #itms <- as.list(as.data.frame(t(oneUsr$itms)))
-     tmp <- match(oneUsr$itms[[1]], newItem) 
+     tmp <- match(oneUsr$itms[[1]], newItem)
       if (all(is.na(tmp))) {
          stop('no data on this item')
       }
       whichOne <- which(!is.na(tmp))
       c(whichOne,oneUsr$ratings[whichOne])
    }
+
    found <- as.matrix(sapply(origData,checkNewItem))
    # found is of dimensions 2 x number of users;
    # found[1,i] = j means origData[[i]]$itms[j] = newItem;
    # found[1,i] = NA means newItem wasn't rated by user i;ß
    # found[2,i] will be the rating in the non-NA case
-   # we need to get rid of the NA users 
+   # we need to get rid of the NA users
    whoHasIt <- which(!is.na(found[1,]))
    # whoHasIt[i] is the index, i.e. user ID, of the i-th user who has
    # rated newData
    if (is.null(whoHasIt) | length(whoHasIt)==0) return(NA)  # no one rated this item
-   origData <- origData[whoHasIt]  
+   origData <- origData[whoHasIt]
    # now origData only has the relevant users, the ones who have rated
    # newItem, so select only those columns of the found matrix
    found <- found[,whoHasIt,drop=FALSE]
@@ -63,10 +57,10 @@ browser()
    # find the distance from newData to one user y of origData; defined for
    # use in sapply() below
    onecos <- function(y) cosDist(newData,y,wtcovs,wtcats)
-   cosines <- sapply(origData,onecos) 
+   cosines <- sapply(origData,onecos)
    # the vector cosines contains the distances from newData to all the
    # original data points;
-  
+
    #action of findKnghbourRtng(): predict rating based on each k[i] neighbours
    #x = k[i]
    #if x > neighbours present in the dataset, then the maximum number of meighbours is used
@@ -80,9 +74,16 @@ browser()
    sapply(k, findKnghbourRtng)
 }
 
-# cosDist() find cosine distance between x and y, elements of an object
-# of 'usrData' class; only items rated in both x and y are used; if none
-# exist, then return NaN
+#'find cosine distance between x and y, elements of an object
+#' of 'usrData' class
+#'
+#'  \code{cosDist} find cosine distance between x and y, elements of an object
+#'   of 'usrData' class; only items rated in both x and y are used; if none
+#'   exist, then return NaN
+#'   @x: object of usrData class
+#'   @y: second object of usrData class
+#'  @wtcovs: weight to put on covariates; NULL if no covs
+#'  @wtcats: weight to put on item categories; NULL if no cats
 cosDist <- function(x,y,wtcovs,wtcats) {
   # rated items in common
    commItms <- intersect(x$itms[[1]],y$itms[[1]])
