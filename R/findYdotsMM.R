a46dadacbde057e67112a481db93628ffeb96e38
Pooja-Rajkumar <pooja.rajkumar123@gmail.com>
2017-06-06 23:51:25 -0700

    added predict.nm, getAcc, and getDiff

--- R/findYdotsMM.R
+++ R/findYdotsMM.R
@@ -115,27 +115,46 @@ checkyd <- function() {
 
 
 
-buildMatrix <- function(ratingsIn,NAval=NA){
-  # deal with possible factors
-  dmax <- function(d) {
-    if (is.factor(d)) return(length(levels(d)))
-    max(d)
-  }
-  users = ratingsIn[,1]
-  movies = ratingsIn[,2]
-  rating = ratingsIn[,3]
-  newMatrix = matrix(NAval, 
-                     nrow = dmax(users), ncol = dmax(movies))
-  for(rows in 1:nrow(ratingsIn)){
-    newMatrix[ratingsIn[rows,1],ratingsIn[rows,2]] = ratingsIn[rows,3]
-  }
-  return (newMatrix)
+predict.NM = function(ydotsObj,testSet) {
+  testSet$pred = ydotsObj$usrMeans[as.character(testSet[,1])] + 
+    ydotsObj$itmMeans[as.character(testSet[,2])] - ydotsObj$grandMean
+#  if (!is.null(ydotsObj$regObj))
+ #   testSet$pred = testSet$pred + predict(ydotsObj$regObj,testSet[,-(1:2)])
+  testSet$pred
 }
 
-# Wrapper for recosystem 
+getDiff <- function(fullMatrix, filledNMF){
+  indicies <- which(!is.na(fullMatrix), arr.ind = TRUE)
+  diff <- matrix(data = NA,nrow = nrow(indicies), 1)
+  for(i in 1:nrow(indicies))
+    for(j in 1:ncol(indicies)){
+      diff[i,1] <- round(fullMatrix[indicies[i,1],indicies[j,2]]) - filledNMF[indicies[i,1],indicies[j,2]]
+    }
+  
+  diff
+}
 
-trainNM <- function(ratingsIn, trainprop = 0.5,cls = NULL,
-                    rnk = 10, recosystem = FALSE,regressYdots=FALSE){
+
+getAcc <- function(fullMatrix,filledNMF, threshold = 0.5) {
+  diff <- abs(getDiff(fullMatrix, filledNMF))
+  avgDiff <- colMeans(abs(diff), na.rm = TRUE, dims =1) # average difference is 1.19 
+  # Remove the na's from diff 
+  # Calculate the values less than the threshold 
+  count = 0;
+  tester <- na.exclude(diff)
+  for(i in 1:nrow(diff))
+  {
+    if(diff[i] < 0.5 )
+      count = count + 1
+  }
+  acc <- count/ nrow(diff)
+  acc
+  
+}
+
+train.NM <- function(ratingsIn, trainprop = 0.5,cls = NULL,
+                    rnk = 10, recosystem = FALSE,regressYdots=FALSE)
+  {
   require(NMF)
   library(NMF)
   if(recosystem == TRUE){
@@ -144,19 +163,24 @@ trainNM <- function(ratingsIn, trainprop = 0.5,cls = NULL,
     res <- xvalReco(ratingsIn,trainprop,cls,rnk)
   }
   else {
-    fullMatrix <- buildMatrix(ratingsIn) # Matrix A (Step 1)
-    
+    origMatrix <- buildMatrix(ratingsIn) # Matrix A (Step 1)
+    fullMatrix <- origMatrix
     approxMatrix <- findYdotsMM(ratingsIn) # Matrix V (Step 2)
     
     naMatrix <- as.data.frame(which(is.na(fullMatrix) == TRUE, arr.ind = TRUE))
     naMatrix$ratings <- NA
-    preds <- predict.ydotsMM(approxMatrix,naMatrix) # Step 3
+    
+    preds <- predict.NM(approxMatrix,naMatrix) # Step 3
+
     fullMatrix[which(is.na(fullMatrix))] <- preds 
     fullMatrix[fullMatrix < 0] <- 0
     require(NMF)
-    res <- nmf(as.matrix(fullMatrix),10) # Step 4
+    result <- nmf(as.matrix(fullMatrix),10) # Step 4
+    filledNMF <- as.matrix(result@fit@W) %*% as.matrix(result@fit@H)
+    avgd <- getAcc(fullMatrix, filledNMF) # 1.149 for the movielens 100k data 
+    
   }
-  res
+  filledNMF
 }
 
 
