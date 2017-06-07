241106838a889477afc97925728490cf28efedad
Pooja-Rajkumar <pooja.rajkumar123@gmail.com>
2017-06-06 23:54:03 -0700

    forgot to add buildmatrix

--- R/findYdotsMM.R
+++ R/findYdotsMM.R
@@ -152,6 +152,23 @@ getAcc <- function(fullMatrix,filledNMF, threshold = 0.5) {
   
 }
 
+buildMatrix <- function(ratingsIn,NAval=NA){
+  # deal with possible factors
+  dmax <- function(d) {
+    if (is.factor(d)) return(length(levels(d)))
+    max(d)
+  }
+  users = ratingsIn[,1]
+  movies = ratingsIn[,2]
+  rating = ratingsIn[,3]
+  newMatrix = matrix(NAval, 
+                     nrow = dmax(users), ncol = dmax(movies))
+  for(rows in 1:nrow(ratingsIn)){
+    newMatrix[ratingsIn[rows,1],ratingsIn[rows,2]] = ratingsIn[rows,3]
+  }
+  return (newMatrix)
+}
+
 train.NM <- function(ratingsIn, trainprop = 0.5,cls = NULL,
                     rnk = 10, recosystem = FALSE,regressYdots=FALSE)
   {
